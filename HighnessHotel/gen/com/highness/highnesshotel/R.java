b25bddc5d4496639b4a362ffb3ebad31b16a0004
Arti <artipatel9105@gmail.com>
2015-03-30 12:41:26 +0530

    Added login and signup activities

--- HighnessHotel/gen/com/highness/highnesshotel/R.java
+++ HighnessHotel/gen/com/highness/highnesshotel/R.java
@@ -25,20 +25,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080000;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_splash=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_sign_up=0x7f030001;
+        public static final int activity_splash=0x7f030002;
     }
     public static final class menu {
-        public static final int splash=0x7f070000;
+        public static final int login=0x7f070000;
+        public static final int sign_up=0x7f070001;
+        public static final int splash=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_login=0x7f050003;
+        public static final int title_activity_sign_up=0x7f050004;
     }
     public static final class style {
         /** 
