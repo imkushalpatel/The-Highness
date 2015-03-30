6ba9be0c3bd01af04486bf400c7f90c134b3562a
Arti <artipatel9105@gmail.com>
2015-03-30 19:47:32 +0530

    created session and json parser

--- HighnessHotel/src/com/highness/highnesshotel/Splash.java
+++ HighnessHotel/src/com/highness/highnesshotel/Splash.java
@@ -1,16 +1,31 @@
 package com.highness.highnesshotel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Splash extends Activity {
+	Button btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		btn=(Button)findViewById(R.id.button1);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i=new Intent(getApplicationContext(),Login.class);
+				startActivity(i);
+			}
+		});
+		
 	}
 
 	@Override
