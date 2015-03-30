3d22ca2ce90665e200eae745e5fb02b47e68f69f
Arti <artipatel9105@gmail.com>
2015-03-30 22:19:22 +0530

    Today's Pull

--- HighnessHotel/src/com/highness/highnesshotel/Login.java
+++ HighnessHotel/src/com/highness/highnesshotel/Login.java
@@ -1,16 +1,40 @@
 package com.highness.highnesshotel;
 
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+
+import helper.JSONParser;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends Activity {
+	Button btn;
+	EditText user,pass;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		btn=(Button)findViewById(R.id.login);
+		user=(EditText)findViewById(R.id.et_username);
+		pass=(EditText)findViewById(R.id.et_password);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				new LoginCheck().execute();
+			}
+		});
 	}
 
 	@Override
@@ -31,4 +55,38 @@ public class Login extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+private class LoginCheck extends AsyncTask<Void, Void, Void>
+{
+
+	ProgressDialog progress;
+	JSONParser jsonparser =new JSONParser();
+	@Override
+	protected void onPreExecute() {
+		// TODO Auto-generated method stub
+		super.onPreExecute();
+		progress= new ProgressDialog(Login.this);
+		progress.setMessage("Validating User...");
+		progress.setIndeterminate(false);
+		progress.setCancelable(false);
+		progress.show();
+		
+	}
+
+	
+	@Override
+	protected void onPostExecute(Void result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+		progress.cancel();
+	}
+
+	@Override
+	protected Void doInBackground(Void... params) {
+		// TODO Auto-generated method stub
+		//List<NameValuePair> param=
+		return null;
+	}
+	
+}
 }
